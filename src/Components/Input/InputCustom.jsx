cbcf47a9ed6118968b6033932cba009c2d114e8e
Đỗ Quang Khải <88335848+QuangKhai-dev@users.noreply.github.com>
2024-03-05 14:42:38 +0700

    layout page

--- src/Components/Input/InputCustom.jsx
+++ src/Components/Input/InputCustom.jsx
@@ -1,14 +1,20 @@
 // eslint-disable-next-line react/prop-types
-const InputCustom = ({ placeholder, onChange, onBlur, value }) => {
+const InputCustom = ({ placeholder, onChange, onBlur, value, id, label }) => {
   return (
     <div>
+      {label ? (
+        <label className="text-sm mb-2 inline-block" htmlFor={id}>
+          {label}
+        </label>
+      ) : null}
       <input
         onChange={onChange}
         onBlur={onBlur}
-        className="py-2 px-4 rounded border border-gray-400 hover:border-black focus:border-black focus-visible:outline-none w-full"
+        className="py-3 px-4 rounded border border-gray-400 hover:border-black focus:border-black focus-visible:outline-none w-full"
         type="text"
         placeholder={placeholder}
         value={value}
+        id={id}
       />
     </div>
   );
--- src/pages/GetInfoRegister/GetInfoRegister.jsx
+++ src/pages/GetInfoRegister/GetInfoRegister.jsx
@@ -1,25 +1,40 @@
-import React from "react";
 import imgRegister from "./../../assets/img/img_getInfoRegister.avif";
 import imgLogoMini from "./../../assets/img/logoMiniMonday.png";
 import InputCustom from "../../Components/Input/InputCustom";
 const GetInfoRegister = () => {
   return (
     <div className="h-screen flex">
-      <div className="info_register_content w-7/12">
-        {/* logo  */}
-        <div>
-          <img src={imgLogoMini} alt="" />
-        </div>
-        {/* input  */}
-        <div>
-          <h1>Create your account</h1>
-          <InputCustom placeholder="Enter your full name" />
-          <InputCustom placeholder="Enter at least 8 characters" />
-          <InputCustom placeholder="For example, company's or department's name" />
-        </div>
-        {/* action redirect  */}
-        <div>
-          <button>Continue</button>
+      <div className="info_register_content w-7/12 px-32 py-16">
+        <div className="flex flex-col h-full">
+          {/* logo  */}
+          <div className="flex-1">
+            <img className="h-6" src={imgLogoMini} alt="" />
+          </div>
+          {/* input  */}
+          <div className="register_input">
+            <h1 className="text-2xl">Create your account</h1>
+            <div className="w-3/4 space-y-5">
+              <InputCustom
+                placeholder="Enter your full name"
+                label="Full name"
+              />
+              <InputCustom
+                placeholder="Enter at least 8 characters"
+                label="Password"
+              />
+              <InputCustom
+                placeholder="For example, company's or department's name"
+                label="Account name"
+              />
+            </div>
+          </div>
+          {/* action redirect  */}
+          <div className="text-right">
+            <button className="bg-blue-500 py-2 px-5 rounded text-white font-light">
+              Continue
+              <i className="fa-solid fa-chevron-right ml-2 text-xs"></i>
+            </button>
+          </div>
         </div>
       </div>
       <div className="info_register_img w-5/12">
