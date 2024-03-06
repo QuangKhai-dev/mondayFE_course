19a4f0e2d73678dec95aad9a49601938865d552a
Đỗ Quang Khải <88335848+QuangKhai-dev@users.noreply.github.com>
2024-03-06 14:35:21 +0700

    setup formik and yup manage form

--- src/Components/Input/InputCustom.jsx
+++ src/Components/Input/InputCustom.jsx
@@ -1,15 +1,32 @@
+/* eslint-disable react/prop-types */
 // eslint-disable-next-line react/prop-types
-const InputCustom = ({ placeholder, onChange, onBlur, value }) => {
+const InputCustom = ({
+  placeholder,
+  onChange,
+  onBlur,
+  value,
+  name,
+  error,
+  touched,
+}) => {
   return (
     <div>
       <input
         onChange={onChange}
         onBlur={onBlur}
-        className="py-2 px-4 rounded border border-gray-400 hover:border-black focus:border-black focus-visible:outline-none w-full"
+        className={`py-2 px-4 rounded border focus-visible:outline-none w-full ${
+          error && touched
+            ? "border-red-500 hover:border-red-500 focus:border-red-500"
+            : "border-gray-400 hover:border-black focus:border-black"
+        }`}
         type="text"
         placeholder={placeholder}
         value={value}
+        name={name}
       />
+      {error && touched ? (
+        <p className="text-red-500 text-xs text-left mt-1">{error}</p>
+      ) : null}
     </div>
   );
 };
--- src/pages/SignUpNew/SignUpNew.jsx
+++ src/pages/SignUpNew/SignUpNew.jsx
@@ -3,7 +3,23 @@ import welcomeImage from "./../../assets/img/welcome-to-monday.avif";
 import ButtonGoogle from "../../Components/Button/ButtonGoogle";
 import InputCustom from "../../Components/Input/InputCustom";
 import ButtonCustom from "../../Components/Button/ButtonCustom";
+import { useFormik } from "formik";
+import * as Yup from "yup";
 const SignUpNew = () => {
+  const { handleChange, handleBlur, values, errors, touched, handleSubmit } =
+    useFormik({
+      initialValues: {
+        email: "",
+      },
+      onSubmit: (values) => {
+        console.log(values);
+      },
+      validationSchema: Yup.object({
+        email: Yup.string()
+          .email("Vui lòng nhập đúng định dạng email")
+          .required("Vui lòng không bỏ trống"),
+      }),
+    });
   return (
     <div className="h-screen flex">
       {/* content  */}
@@ -13,25 +29,36 @@ const SignUpNew = () => {
             <h1 className="text-3xl mb-3">Welcome to monday.com</h1>
             <p>Get started - it{"'"}s free. No credit card needed.</p>
           </div>
-          <div className="signup_action space-y-5">
-            <ButtonGoogle />
-            <div className="flex items-center space-x-3">
-              <div className="line_cross flex-1 h-[1px] bg-gray-400"></div>
-              <span>Or</span>
-              <div className="line_cross flex-1 h-[1px] bg-gray-400"></div>
-            </div>
-            <InputCustom placeholder="name@company.com" />
-            <ButtonCustom content="Continue" />
-            <p className="space-x-2 text-sm">
-              By proceeding, you agree to the <br />
-              <a className="style_a" href="#">
-                Terms of Service
-              </a>
-              <span>and</span>
-              <a className="style_a" href="#">
-                Privacy Policy
-              </a>
-            </p>
+          <div className="signup_action">
+            <form onSubmit={handleSubmit} className="space-y-5">
+              <ButtonGoogle />
+              <div className="flex items-center space-x-3">
+                <div className="line_cross flex-1 h-[1px] bg-gray-400"></div>
+                <span>Or</span>
+                <div className="line_cross flex-1 h-[1px] bg-gray-400"></div>
+              </div>
+              <InputCustom
+                onChange={handleChange}
+                onBlur={handleBlur}
+                value={values.email}
+                name="email"
+                placeholder="name@company.com"
+                error={errors.email}
+                touched={touched.email}
+              />
+
+              <ButtonCustom content="Continue" type="submit" />
+              <p className="space-x-2 text-sm">
+                By proceeding, you agree to the <br />
+                <a className="style_a" href="#">
+                  Terms of Service
+                </a>
+                <span>and</span>
+                <a className="style_a" href="#">
+                  Privacy Policy
+                </a>
+              </p>
+            </form>
           </div>
           <p className="mb-10 text-sm">
             Already have an account?{" "}
