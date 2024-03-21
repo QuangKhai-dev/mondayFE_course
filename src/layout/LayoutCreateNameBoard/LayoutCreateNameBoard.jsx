2d484df94c90851cbd332aa05f80936dcf0fa1c2
Đỗ Quang Khải <88335848+QuangKhai-dev@users.noreply.github.com>
2024-03-21 16:49:29 +0700

    feat: handle layout create name board

--- src/layout/LayoutCreateNameBoard/LayoutCreateNameBoard.jsx
+++ src/layout/LayoutCreateNameBoard/LayoutCreateNameBoard.jsx
@@ -1,16 +1,31 @@
-import React from "react";
+import React from 'react';
 
 const LayoutCreateNameBoard = ({ handleStep }) => {
   return (
-    <div>
-      LayoutCreateNameBoard
-      <button
-        onClick={() => {
-          handleStep(1);
-        }}
-      >
-        Next
-      </button>
+    <div className="text-title">
+      {/* title  */}
+      <h1 className="text-2xl mb-5">Let's start working together</h1>
+      <p className="text-sm mb-3">
+        Give your board a name, e.g. marketing plan, sales pipeline, quarterly
+        roadmap...
+      </p>
+      {/* input  */}
+      <div className="px-5 py-2 border border-gray-300 rounded relative hover:border-black duration-500 mb-10">
+        <input
+          className="focus:outline-none"
+          type="text"
+          placeholder="My First Board"
+        />
+        <button className="absolute right-2 top-1/2 -translate-y-1/2 h-3/4 w-7 rounded hover:bg-slate-200">
+          X
+        </button>
+      </div>
+      {/* description  */}
+      <div className="bg-slate-100 p-5 rounded-md text-[15px]">
+        <p>
+          In monday.com, "boards" are the place where all your content lives.
+        </p>
+      </div>
     </div>
   );
 };
--- src/templates/CreateBoardTemplate/CreateBoardTemplate.jsx
+++ src/templates/CreateBoardTemplate/CreateBoardTemplate.jsx
@@ -1,10 +1,10 @@
-import React, { useState } from "react";
-import LayoutCreateNameBoard from "../../layout/LayoutCreateNameBoard/LayoutCreateNameBoard";
-import LayoutCreateColumnBoard from "../../layout/LayoutCreateColumnBoard/LayoutCreateColumnBoard";
-import logoMiniCreateBoard from "./../../assets/img/logoMiniCreateBoard.png";
+import React, { useState } from 'react';
+import LayoutCreateNameBoard from '../../layout/LayoutCreateNameBoard/LayoutCreateNameBoard';
+import LayoutCreateColumnBoard from '../../layout/LayoutCreateColumnBoard/LayoutCreateColumnBoard';
+import logoMiniCreateBoard from './../../assets/img/logoMiniCreateBoard.png';
 const CreateBoardTemplate = () => {
   const [step, setStep] = useState(0);
-  const handleStep = (number) => {
+  const handleStep = number => {
     // -1
     // 1
     setStep(step + number);
@@ -20,12 +20,16 @@ const CreateBoardTemplate = () => {
   return (
     <div className="h-screen flex">
       {/* content board  */}
-      <div className="w-1/2 py-16 px-32">
+      <div className="w-1/2 py-16 px-52 flex flex-col items-center justify-center">
         {/* logo  */}
-        <div className="create_board_logo">
-          <img className="h-6" src={logoMiniCreateBoard} alt="" />
+        <div className="create_board_logo w-full mb-20">
+          <img className="h-6 " src={logoMiniCreateBoard} alt="" />
         </div>
         {renderCreateBoard()}
+        <div>
+          <button>Back</button>
+          <button>Next</button>
+        </div>
       </div>
       {/* view board  */}
       <div className="w-1/2 bg-orange-500"></div>
