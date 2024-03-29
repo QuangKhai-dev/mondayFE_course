6e0ecc3710aeaef6cdeeab95ba924706db788832
Đỗ Quang Khải <88335848+QuangKhai-dev@users.noreply.github.com>
2024-03-29 10:44:35 +0700

    feat layout home template

--- src/routes/useRoutesCustom.jsx
+++ src/routes/useRoutesCustom.jsx
@@ -1,8 +1,9 @@
-import { useRoutes } from "react-router-dom";
-import GetInfoRegister from "../pages/GetInfoRegister/GetInfoRegister";
-import SurveyQuestion from "../pages/SurveyQuestion/SurveyQuestion";
-import InviteMember from "../pages/InviteMember/InviteMember";
-import CreateBoardTemplate from "../templates/CreateBoardTemplate/CreateBoardTemplate";
+import { useRoutes } from 'react-router-dom';
+import GetInfoRegister from '../pages/GetInfoRegister/GetInfoRegister';
+import SurveyQuestion from '../pages/SurveyQuestion/SurveyQuestion';
+import InviteMember from '../pages/InviteMember/InviteMember';
+import CreateBoardTemplate from '../templates/CreateBoardTemplate/CreateBoardTemplate';
+import HomeTemplate from '../templates/HomeTemplate/HomeTemplate';
 const useRoutesCustom = () => {
   let element = useRoutes([
     // {
@@ -10,8 +11,8 @@ const useRoutesCustom = () => {
     //   element: <InviteMember />,
     // },
     {
-      path: "/",
-      element: <CreateBoardTemplate />,
+      path: '/',
+      element: <HomeTemplate />,
     },
   ]);
 
---
+++ src/templates/HomeTemplate/HomeTemplate.jsx
@@ -0,0 +1,93 @@
+import React, { useState } from 'react';
+import {
+  DesktopOutlined,
+  FileOutlined,
+  PieChartOutlined,
+  TeamOutlined,
+  UserOutlined,
+} from '@ant-design/icons';
+import { Breadcrumb, Layout, Menu, theme } from 'antd';
+const { Header, Content, Footer, Sider } = Layout;
+function getItem(label, key, icon, children) {
+  return {
+    key,
+    icon,
+    children,
+    label,
+  };
+}
+const items = [
+  getItem('Option 1', '1', <PieChartOutlined />),
+  getItem('Option 2', '2', <DesktopOutlined />),
+  getItem('User', 'sub1', <UserOutlined />, [
+    getItem('Tom', '3'),
+    getItem('Bill', '4'),
+    getItem('Alex', '5'),
+  ]),
+  getItem('Team', 'sub2', <TeamOutlined />, [
+    getItem('Team 1', '6'),
+    getItem('Team 2', '8'),
+  ]),
+  getItem('Files', '9', <FileOutlined />),
+];
+const HomeTemplate = () => {
+  const [collapsed, setCollapsed] = useState(false);
+  const {
+    token: { colorBgContainer, borderRadiusLG },
+  } = theme.useToken();
+  return (
+    <Layout
+      style={{
+        minHeight: '100vh',
+        backgroundColor: '#ECEFF8',
+      }}
+    >
+      <Header
+        style={{
+          padding: 0,
+          background: '#ECEFF8',
+        }}
+      />
+
+      <Layout style={{ background: '#ECEFF8' }}>
+        <Sider
+          collapsible
+          collapsed={collapsed}
+          onCollapse={value => setCollapsed(value)}
+          style={{
+            background: 'white',
+            borderTopRightRadius: '8px',
+          }}
+          width="15%"
+        >
+          <div className="demo-logo-vertical" />
+          {/* <Menu
+            theme="dark"
+            defaultSelectedKeys={['1']}
+            mode="inline"
+            items={items}
+          /> */}
+        </Sider>
+        <Content
+          style={{
+            margin: '0 16px',
+            width: '100%',
+          }}
+        >
+          <div
+            style={{
+              padding: 24,
+              minHeight: '100%',
+              background: colorBgContainer,
+              borderRadius: borderRadiusLG,
+            }}
+          >
+            Bill is a cat.
+          </div>
+        </Content>
+      </Layout>
+    </Layout>
+  );
+};
+
+export default HomeTemplate;
